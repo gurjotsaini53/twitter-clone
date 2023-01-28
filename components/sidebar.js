f8c24de93b372471d3ae46a59093ec008f74f235
gurjot <gurjotsaini53@gmail.com>
2023-01-28 23:13:58 +0530

    working on sidebar

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,7 +1,10 @@
 function sidebar() {
     return (
-        <div>
-            <p>sidebar</p>
+        <div className="hidden sm:flex flex-col item-center xl:items-start
+        xl:w-[340px] p-2 fixed h-full">
+            <div className="flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24 ">
+                <Image src="https://rb.gy/ogau5a" width="30" height="30" />
+            </div>
         </div>
     )
 }
